e2cfb23a63c31baae81860973fde7f2aac4f7b7f
fernandoemconstrucao <fernandoemconstrucao@gmail.com>
2026-08-20 18:08:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ exchange = ccxt.bingx()
 def enviar_mensagem_telegram(mensagem):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    {TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": mensagem}
     requests.post(url, json=payload)
 
